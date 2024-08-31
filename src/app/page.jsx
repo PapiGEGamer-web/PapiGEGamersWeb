cb6c9186d5e8c51bdc240f4fd1265b0c85cabe63
Pablo Schefer Orduña <pablopme42@gmail.com>
2024-08-31 03:29:51 +0200

    otra foto

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -131,7 +131,7 @@ export default function Component() {
             </div>
           </div>
           <img
-            src="/placeholder.svg"
+            src="https://cdn.discordapp.com/avatars/979165049258143777/d0107a00f718473260af2f86e576bc09.webp?size=4096"
             width="1200"
             height="600"
             alt="Intro"
@@ -214,7 +214,7 @@ export default function Component() {
               </CardContent>
               <CardFooter className="flex justify-end">
                 <Link
-                  href="https://cdn.discordapp.com/avatars/979165049258143777/d0107a00f718473260af2f86e576bc09.webp?size=4096"
+                  href="#"
                   className="inline-flex h-9 items-center justify-center rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:scale-105 duration-300"
                   prefetch={false}>
                   Ver más
