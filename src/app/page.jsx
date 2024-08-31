0e8aa13cc7af8213a5b70eca7dcd3408b85795da
Pablo Schefer Orduña <pablopme42@gmail.com>
2024-08-31 03:26:13 +0200

    He añaido una foto

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -120,7 +120,7 @@ export default function Component() {
                 href="#"
                 className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:scale-105 duration-300"
                 prefetch={false}>
-                Ver proyectosmiau
+                Ver proyectos
               </Link>
               <Link
                 href="#"
@@ -214,7 +214,7 @@ export default function Component() {
               </CardContent>
               <CardFooter className="flex justify-end">
                 <Link
-                  href="#"
+                  href="https://cdn.discordapp.com/avatars/979165049258143777/d0107a00f718473260af2f86e576bc09.webp?size=4096"
                   className="inline-flex h-9 items-center justify-center rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:scale-105 duration-300"
                   prefetch={false}>
                   Ver más
