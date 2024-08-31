d4b81896485feaa1a46af8e0201890c9f482e377
Pablo Schefer Orduña <pablopme42@gmail.com>
2024-08-31 03:01:55 +0200

    miau

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -120,7 +120,7 @@ export default function Component() {
                 href="#"
                 className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:scale-105 duration-300"
                 prefetch={false}>
-                Ver proyectos
+                Ver proyectosmiau
               </Link>
               <Link
                 href="#"
